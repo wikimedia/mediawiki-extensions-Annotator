1ab8f436ed0c32b16a9e82ade9f18e91521880e7
Richa <richa.jain1912@gmail.com>
2013-06-28 18:28:15 +0530

    Added the api endpoint and called the store plugin.
    Change-Id: I86a87e4fdfe027767f938dc2751e1f2d82b063f4

--- modules/Annotator.js
+++ modules/Annotator.js
@@ -3,7 +3,19 @@
 */
 ( function ( mw, $ ) {
   $( function( $ ) {
-  	$('#mw-content-text').annotator();
+    //get the endpoint of the api
+    this.apiUrl = mw.util.wikiScript('api');
+    //Call the annotations
+    var annotations = $('#mw-content-text').annotator();
+    //Add the store plugin and modify the urls according to mediawiki api
+    annotations.annotator('addPlugin', 'Store', {
+      prefix: this.apiUrl,
+      urls: {
+        create: '',
+        update: '',
+        read: '',
+        destroy: '',
+      }
+    });
   } )
 }( mediaWiki, jQuery ) );
-
