f54554487d6122c1a24c494062dc3d28b47ec8be
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2013-07-26 12:55:57 +0530

    Code format cleanup
    As per http://www.mediawiki.org/wiki/Manual:Coding_conventions/JavaScript
    
    Change-Id: Ieb1bdf9716cb20f1653d87e3f1581eb7adfeecb9

--- modules/Annotator.js
+++ modules/Annotator.js
@@ -1,27 +1,27 @@
-/* Script for calling the annotations
+/**
+ * Script for calling the annotations
+ */
+( function( mw, $ ) {
+	$( function( $ ) {
+		var revid, annotations;
 
-*/
-( function ( mw, $ ) {
-  $( function( $ ) {
-    //get the endpoint of the api
-    this.apiUrl = mw.util.wikiScript('api');
-    //Get the Revision Id of the page
-    var revid = mw.config.get( 'wgCurRevisionId' );
-    //Call the annotations
-    var annotations = $('#mw-content-text').annotator();
-    //Add the store plugin and modify the urls according to mediawiki api
-    annotations.annotator('addPlugin', 'Store', {
-      prefix: this.apiUrl,
-      urls: {
-        create: '?action=annotator-create&format=json&revid=' + revid,
-        update: '',
-        read: '?action=annotator-read&format=json&id=:id',
-        destroy: '?action=annotator-destroy&format=json&id=:id',
-        search: '?action=annotator-search&format=json'
-      },
-      loadFromSearch: {
-        'revid': revid
-      }
-    });
-  } )
+		//Get the Revision Id of the page
+		revid = mw.config.get( 'wgCurRevisionId' );
+		//Call the annotations
+		annotations = $( '#mw-content-text' ).annotator();
+		//Add the store plugin and modify the urls according to mediawiki api
+		annotations.annotator( 'addPlugin', 'Store', {
+			prefix: mw.util.wikiScript( 'api' ),
+			urls: {
+				create: '?action=annotator-create&format=json&revid=' + revid,
+				update: '',
+				read: '?action=annotator-read&format=json&id=:id',
+				destroy: '?action=annotator-destroy&format=json&id=:id',
+				search: '?action=annotator-search&format=json'
+			},
+			loadFromSearch: {
+				revid: revid
+			}
+		} );
+	} );
 }( mediaWiki, jQuery ) );
