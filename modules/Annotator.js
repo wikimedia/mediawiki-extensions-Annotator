540d5584c13c4709aab552623f1e58fe6e55b0ae
Richa <richa.jain1912@gmail.com>
2013-08-04 23:46:36 +0530

    Allow logged out users to view
    (bug 52260)
    Change-Id: I3b7174ec78c9419fe56871dc46fc0f06826dd138

--- modules/Annotator.js
+++ modules/Annotator.js
@@ -10,7 +10,12 @@
 		//Get the userId
 		userId = mw.config.get( 'wgUserId' );
 		//Call the annotations
-		annotations = $( '#mw-content-text' ).annotator();
+		if( userId === null ) {
+			annotations = $( '#mw-content-text' ).annotator({ readOnly: true });
+		}
+		else {
+			annotations = $( '#mw-content-text' ).annotator();
+		}
 		//Add the store plugin and modify the urls according to mediawiki api
 		annotations.annotator( 'addPlugin', 'Store', {
 			prefix: mw.util.wikiScript( 'api' ),
@@ -26,32 +31,32 @@
 			}
 		} );
 
-    //add the permissions plugin
-    annotations.annotator('addPlugin', 'Permissions', {
-      
-      user: {
-        id: userId,
-        username: mw.user.getName()
-      },
-      permissions: {
-        'read': [],
-        'update': [ userId ],
-        'delete': [ userId ]
-      },
-      userId: function (user) {
-        if( user && user.id ) {
-          return user.id;
-        }
-        return user;
-      },
-      userString: function (user) {
-        if( user && user.username ) {
-          return user.username;
-        }
-        return user;
-      },
-      showViewPermissionsCheckbox: false,
-      showEditPermissionsCheckbox: false
-    });
-  } )
+		//add the permissions plugin
+		annotations.annotator('addPlugin', 'Permissions', {
+			
+			user: {
+				id: userId,
+				username: mw.user.getName()
+			},
+			permissions: {
+				'read': [],
+				'update': [ userId ],
+				'delete': [ userId ]
+			},
+			userId: function (user) {
+				if( user && user.id ) {
+					return user.id;
+				}
+				return user;
+			},
+			userString: function (user) {
+				if( user && user.username ) {
+					return user.username;
+				}
+				return user;
+			},
+			showViewPermissionsCheckbox: false,
+			showEditPermissionsCheckbox: false
+		});
+	} )
 }( mediaWiki, jQuery ) );
